c0a3427846d78faf76583340205b1ba6610beb5c
zetavg <mail@zeta.vg>
2021-06-10 00:55:12 +0800

    let init supports running on specific target

--- functions/src/init.ts
+++ functions/src/init.ts
@@ -1,10 +1,17 @@
 /**
  * Run some initialize code at a proper time.
  */
-export function init(fn: () => void): void {
-  if (process.env.DO_INIT !== 'FALSE') {
-    setTimeout(fn, 0);
-  }
+export function init(fn: () => void, onlyOnTarget?: string): void {
+  if (process.env.DO_INIT === 'FALSE') return;
+
+  if (
+    onlyOnTarget &&
+    process.env.FUNCTION_TARGET &&
+    process.env.FUNCTION_TARGET !== onlyOnTarget
+  )
+    return;
+
+  setTimeout(fn, 0);
 }
 
 export default init;
