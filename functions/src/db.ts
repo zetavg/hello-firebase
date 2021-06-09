85a9fe4ebeab44d43aec42b216bdfcae4f7bed73
zetavg <mail@zeta.vg>
2021-06-09 22:40:27 +0800

    add firestore samples

--- functions/src/db.ts
+++ functions/src/db.ts
@@ -1,6 +1,12 @@
 import admin from 'firebase-admin';
+import { Firestore } from '@google-cloud/firestore';
 
-export const getDb = (): admin.database.Database => {
+export const getRealtimeDb = (): admin.database.Database => {
   if (!admin.apps.length) admin.initializeApp();
   return admin.database();
+};
+
+export const getFirestore = (): Firestore => {
+  if (!admin.apps.length) admin.initializeApp();
+  return admin.firestore();
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
-import { getDb } from './db';
+import { firestore } from 'firebase-admin';
+import { getFirestore, getRealtimeDb } from './db';
 import init from './init';
 
 init(() => {
@@ -29,7 +30,7 @@ export const getData = functions.https.onRequest(async (request, response) => {
     return;
   }
 
-  const db = getDb();
+  const db = getRealtimeDb();
   const data = await db.ref(`data/${path}`).get();
   response.send(data);
 });
@@ -68,7 +69,7 @@ export const setData = functions.https.onRequest(async (request, response) => {
     return;
   }
 
-  const db = getDb();
+  const db = getRealtimeDb();
   await db.ref(`data/${path}`).set(data);
   response.send({ success: true });
 });
@@ -85,5 +86,88 @@ export const logDataChanges = functions.database
         auth_type: context.authType,
         auth_uid: context.auth?.uid || null,
       },
+    });
+  });
+
+export const getFirestoreData = functions.https.onRequest(
+  async (request, response) => {
+    const { id } = request.query;
+
+    if (typeof id !== 'string') {
+      response.send({ error: `Please specify the query parameter "id".` });
+      return;
+    }
+
+    const db = getFirestore();
+    const doc = await db.collection('data').doc(id).get();
+
+    if (!doc.exists) {
+      response.send('null');
+    } else {
+      response.send(doc.data());
+    }
+  },
+);
+
+export const setFirestoreData = functions.https.onRequest(
+  async (request, response) => {
+    const { id } = request.query;
+
+    if (typeof id !== 'string') {
+      response.send({ error: `Please specify the query parameter "id".` });
+      return;
+    }
+
+    const [data, error] = (() => {
+      const requestQueryData = request.query.data;
+      if (typeof requestQueryData === 'string') {
+        try {
+          return [JSON.parse(requestQueryData), null];
+        } catch (e) {
+          return [
+            undefined,
+            `The query parameter "data" is not a vaild JSON string.`,
+          ];
+        }
+      } else if (request.headers['content-type'] === 'application/json') {
+        return [request.body];
+      }
+
+      return [
+        undefined,
+        `Please pass the data via the query parameter "data" or POST body with "Content-Type: application/json".`,
+      ];
+    })();
+
+    if (error) {
+      response.send({ error });
+      return;
+    }
+
+    const db = getFirestore();
+    try {
+      await db.collection('data').doc(id).set(data);
+      response.send({ success: true });
+    } catch (e) {
+      response.send({ error: e.message });
+    }
+  },
+);
+
+export const logFirestoreDataChanges = functions.firestore
+  .document('data/{documentId}')
+  .onWrite(async (change, context) => {
+    const oldData = change.before.exists ? change.before.data() : null;
+    const data = change.after.exists ? change.after.data() : null;
+
+    return await change.after.ref.firestore.collection('data_changelog').add({
+      docId: context.params.documentId,
+      doc: change.after.ref,
+      before: oldData,
+      after: data,
+      timestamp: firestore.Timestamp.fromDate(new Date(context.timestamp)),
+      event_id: context.eventId,
+      auth_type: context.authType || null,
+      auth_uid: context.auth?.uid || null,
     });
   });
