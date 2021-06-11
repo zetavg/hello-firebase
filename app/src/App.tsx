d73d116d85e13af7fad7b45b0001a4f37ccb1300
zetavg <mail@zeta.vg>
2021-06-11 23:46:38 +0800

    add firebase sample

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
+import { useCallback, useState } from 'react';
+import { firestore } from './firebase';
 import logo from './logo.svg';
 import './App.css';
 
 function App(): JSX.Element {
+  const [dataId, setDataId] = useState('');
+  const [dataJson, setDataJson] = useState('');
+
+  const getData = useCallback(async () => {
+    try {
+      const doc = await firestore.collection('data').doc(dataId).get();
+      setDataJson(JSON.stringify(doc.exists ? doc.data() : null, null, 2));
+    } catch (e) {
+      alert(e.message);
+    }
+  }, [dataId]);
+
+  const setData = useCallback(async () => {
+    try {
+      const data = JSON.parse(dataJson);
+      await firestore.collection('data').doc(dataId).set(data);
+    } catch (e) {
+      alert(e.message);
+    }
+  }, [dataId, dataJson]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section className="firestore">
+          Firestore
+          <div className="input-group">
+            Data ID:{' '}
+            <input value={dataId} onChange={(e) => setDataId(e.target.value)} />
+          </div>
+          <div className="input-group">
+            Data JSON:{' '}
+            <textarea
+              value={dataJson}
+              onChange={(e) => setDataJson(e.target.value)}
+            />
+          </div>
+          <div className="actions">
+            <button onClick={getData}>Get data</button>{' '}
+            <button onClick={setData}>Set data</button>
+          </div>
+        </section>
         <section className="App-info">
           <p>
             My External Service's public key is{' '}
